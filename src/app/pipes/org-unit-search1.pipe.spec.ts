8cabc8b9c468e9f35ff2cc1a3016d717511f059f
parthnayak7448 <parth.nayak@vale.com>
2019-06-18 14:50:10 -0400

    Removed template/sample files.

--- src/app/pipes/org-unit-search1.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OrgUnitSearchPipe } from './org-unit-search.pipe';
-
-describe('OrgUnitSearch1Pipe', () => {
-  it('create an instance', () => {
-    const pipe = new OrgUnitSearchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
